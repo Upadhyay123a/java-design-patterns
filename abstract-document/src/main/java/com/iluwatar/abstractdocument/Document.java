034227ae5ec2e54c1933c5daf552f2ce680bd996
Atul Kumar Upadhyay <137402498+Upadhyay123a@users.noreply.github.com>
2025-06-01 19:37:54 +0530

    Update Document.java
    "Added generic get method and default containsKey method to Document interface for improved type safety and convenience."

--- abstract-document/src/main/java/com/iluwatar/abstractdocument/Document.java
+++ abstract-document/src/main/java/com/iluwatar/abstractdocument/Document.java
@@ -1,27 +1,3 @@
-/*
- * This project is licensed under the MIT license. Module model-view-viewmodel is using ZK framework licensed under LGPL (see lgpl-3.0.txt).
- *
- * The MIT License
- * Copyright © 2014-2022 Ilkka Seppälä
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
 package com.iluwatar.abstractdocument;
 
 import java.util.Map;
@@ -36,24 +12,35 @@ public interface Document {
    *
    * @param key element key
    * @param value element value
-   * @return Void
+   * @return previous value associated with the key or null if none
    */
-  Void put(String key, Object value);
+  Object put(String key, Object value);
 
   /**
    * Gets the value for the key.
    *
+   * @param <T> expected type
    * @param key element key
    * @return value or null
    */
-  Object get(String key);
+  <T> T get(String key);
 
   /**
    * Gets the stream of child documents.
    *
    * @param key element key
    * @param constructor constructor of child class
-   * @return child documents
+   * @return stream of child documents or empty stream if none
    */
   <T> Stream<T> children(String key, Function<Map<String, Object>, T> constructor);
+
+  /**
+   * Checks if the document contains the key.
+   *
+   * @param key element key
+   * @return true if key exists
+   */
+  default boolean containsKey(String key) {
+    return get(key) != null;
+  }
 }
